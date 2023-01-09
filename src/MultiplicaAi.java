dc7038a89c2d9b92087a6c17b54497a0dfcda892
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-01-09 19:00:28 -0300

    feat: multiply exercise

---
+++ src/MultiplicaAi.java
@@ -0,0 +1,11 @@
+public class MultiplicaAi {
+    public static void main(String[] args) {
+        final int numberToMultiply = 9;
+
+        System.out.println("Tabela de multiplicação do 9 :");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println("9 * " + i + " = " + numberToMultiply * i);
+        }
+    }
+}
